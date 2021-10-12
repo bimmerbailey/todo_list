5124d7434f46af934f05e7ab6d7d4bc0103bc8d0
Michael Bailey <steve_mikebailey@hotmail.com>
2021-10-12 15:32:24 -0400

    added vue-material
    drawer was added to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,69 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!--    <div id="nav">-->
+    <!--      <router-link to="/">Home</router-link> |-->
+    <!--      <router-link to="/about">About</router-link>-->
+    <!--    </div>-->
+    <div class="page-container md-layout-column" id="nav">
+      <md-toolbar class="md-primary">
+        <md-button class="md-icon-button" @click="showNavigation = true">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <span class="md-title">TODO App</span>
+
+      </md-toolbar>
+      <md-drawer :md-active.sync="showNavigation" md-swipeable>
+        <md-toolbar class="md-transparent" md-elevation="0">
+          <span class="md-title">My App name</span>
+        </md-toolbar>
+
+        <div class="md-toolbar-section-end">
+          <md-button class="md-icon-button md-dense" @click="toggleMenu">
+            <md-icon>keyboard_arrow_left</md-icon>
+          </md-button>
+        </div>
+
+        <md-list>
+          <md-list-item>
+            <md-icon>move_to_inbox</md-icon>
+            <span class="md-list-item-text">Inbox</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">Sent Mail</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>delete</md-icon>
+            <span class="md-list-item-text">Trash</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>error</md-icon>
+            <span class="md-list-item-text">Spam</span>
+          </md-list-item>
+        </md-list>
+      </md-drawer>
+
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Temporary',
+  data: () => ({
+    showNavigation: false,
+  }),
+  methods: {
+    toggleMenu () {
+      this.showNavigation = !this.showNavigation
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -18,7 +75,7 @@
 }
 
 #nav {
-  padding: 30px;
+  //padding: 30px;
 
   a {
     font-weight: bold;
