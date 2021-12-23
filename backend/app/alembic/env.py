08bac6910820cc10878a3c206017c55561ed3c23
Michael Bailey <steve_mikebailey@hotmail.com>
2021-12-23 07:39:21 -0500

    new alembic revision

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -1,9 +1,6 @@
-import os
-import sys
 from logging.config import fileConfig
 
 from alembic import context
-from dotenv import load_dotenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
